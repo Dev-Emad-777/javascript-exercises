cbfd5e74896e8c0c90d5ade3b12b3c721f541295
Dev-Emad-777 <emadezi777@gmail.com>
2024-02-29 11:02:02 +0300

    fix(03) and configure ESlint

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const strArray = [];
+  for (let i = 0, length = str.length; i < length; i++) {
+    strArray[i] = str[i];
+  }
+  return (strArray.reverse()).join("")
 };
-
 // Do not edit below this line
 module.exports = reverseString;
