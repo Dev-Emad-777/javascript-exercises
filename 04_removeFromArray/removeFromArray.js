634ab4930fd7f54a68875cb97ad3ac94e424bfeb
Dev-Emad-777 <emadezi777@gmail.com>
2024-02-29 12:46:55 +0300

    fix(04) mission completed👌 (●'◡'●)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
+  // loop over every element of arr
+  for (const one of array) {
+    // if args doesn't include arr's element add the element to the newArray
+    if (!args.includes(one)) {
+      newArray.push(one);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
