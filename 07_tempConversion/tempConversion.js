400b061f61bb63c7385326386eed1e20c11a29a2
Dev-Emad-777 <emadezi777@gmail.com>
2024-03-04 00:09:07 +0300

    answer(07): finish TOPs intro js excersise 😊

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+// const { round } = require("prelude-ls");
+
+const convertToCelsius = function (fahrenheit) {
+  // fahrenheit to celsius formula
+  // ? °C = (°F - 32) × 5/9
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return Math.round(celsius * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // celsius degree to fahrenheit formula
+  // ? °F = °C × (9/5) + 32
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return Math.round(fahrenheit *10)/10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
