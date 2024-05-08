c10b10b22660ea6eafc2e38e5fab81f7cf5fc354
Dev-Emad-777 <emadezi777@gmail.com>
2024-05-09 01:03:38 +0300

    fix(09): answered but with a bit of inefficiency 😐

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  // todo: store every char of the text in array
+  const arr = Array.from(text);
 
+  const original = arr.filter((item) => {
+    let regex = /^[A-Za-z0-9]+$/;
+    return regex.test(item);
+  });
+  // todo: copy that array but reversed
+  const reversed = [];
+  original.map((char) => reversed.unshift(char));
+  // todo: compare both arrays values if equal return true
+  return original.join().toLowerCase() == reversed.join().toLowerCase();
 };
 
 // Do not edit below this line
