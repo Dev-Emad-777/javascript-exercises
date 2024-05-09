9f377761cf05e011133712c93493823ce90ea251
Dev-Emad-777 <emadezi777@gmail.com>
2024-05-09 23:32:17 +0300

    fix(11): answer the final js intro exercise 🔥

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((total, current) => {
+    let oldesObj = total;
+    let oldestAge = getAge(oldesObj.yearOfBirth, oldesObj.yearOfDeath);
+    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+    if (currentAge > oldestAge) {
+      oldesObj = current;
+    }
+    return oldesObj;
+  });
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/* 
+people.sort((a, b) => {
+    let age1 = a.yearOfDeath - a.yearOfBrith;
+    let age2 = b.yearOfDeath - b.yearOfBrith;
+    
+  }); */
