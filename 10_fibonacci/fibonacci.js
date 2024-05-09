cd22cf423ef62ab14787d23bca5a743e16ee829e
Dev-Emad-777 <emadezi777@gmail.com>
2024-05-09 23:29:41 +0300

    fix(10): answer fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+// ! Fn = Fn-1 + Fn-2
+const fibonacci = function (n) {
+  if (typeof n !== "number") {
+    n = parseInt(n);
+  }
+  if (typeof n !== "number") {
+    return "OOPS";
+  }
+  if (n < 0) return "OOPS";
 
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
